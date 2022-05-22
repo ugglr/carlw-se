5deb60f05f3cf1d46330720410476f93b4034ca5
Ugglr <carl.igelstrom@gmail.com>
2022-05-22 11:31:43 +0800

    MegaHero animation complete

--- components/MegaHero.tsx
+++ components/MegaHero.tsx
@@ -1,5 +1,6 @@
 import Lottie from "react-lottie";
 import Button from "./Button";
+import { motion } from "framer-motion";
 
 import styles from "./MegaHero.module.scss";
 import scrollingPurpleClouds from "../public/lotties/scrolling-purple-clouds.json";
@@ -21,6 +22,21 @@ type HeroProps = {
   subtitle?: string;
   miniBottom: string;
 };
+const containerAnimationVariant = {
+  visible: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+const topTextAnimationVariant = {
+  visible: { y: 0, transition: { duration: 1 } },
+  hidden: { y: -200 },
+};
+const textAnimationVariant = {
+  visible: { opacity: 1, x: 0, transition: { duration: 0.8 } },
+  hidden: { opacity: 0, x: -100 },
+};
 
 const HomeHero = ({
   miniTitle,
@@ -32,25 +48,63 @@ const HomeHero = ({
   ctaButtonLabel,
   ctaButtonLink,
 }: HeroProps) => (
-  <div className={styles.hero}>
+  <motion.div
+    initial="hidden"
+    animate="visible"
+    variants={containerAnimationVariant}
+    className={styles.hero}
+  >
     <div className={styles.heroContent}>
-      <h2 className={styles.miniTitle}>{miniTitle}</h2>
-      <h1 className={styles.heroTitle}>{megaLine1}</h1>
-      <h1 className={styles.heroTitle}>{megaLine2}</h1>
-      <h2 className={styles.heroSubtitle}>{description}</h2>
-      {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
-      <div className={styles.buttonContainer}>
+      <motion.h2
+        className={styles.miniTitle}
+        variants={topTextAnimationVariant}
+      >
+        {miniTitle}
+      </motion.h2>
+      <motion.h1 variants={textAnimationVariant} className={styles.heroTitle}>
+        {megaLine1}
+      </motion.h1>
+      <motion.h1 className={styles.heroTitle} variants={textAnimationVariant}>
+        {megaLine2}
+      </motion.h1>
+      <motion.h2
+        className={styles.heroSubtitle}
+        variants={textAnimationVariant}
+      >
+        {description}
+      </motion.h2>
+      {subtitle && (
+        <motion.p className={styles.subtitle} variants={textAnimationVariant}>
+          {subtitle}
+        </motion.p>
+      )}
+      <motion.div
+        className={styles.buttonContainer}
+        animate={{ opacity: [0, 1], x: [200, 0] }}
+        transition={{ duration: 1 }}
+      >
         <a href={ctaButtonLink}>
           <Button {...{ title: ctaButtonLabel, variant: "primary" }} />
         </a>
-      </div>
-      <h1>{miniBottom}</h1>
+      </motion.div>
+      <motion.h1
+        animate={{
+          y: [-4, 8],
+        }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "reverse",
+          duration: 1.5,
+        }}
+      >
+        {miniBottom}
+      </motion.h1>
     </div>
 
     <div className={styles.lottieContainer}>
       <Lottie {...{ options: scrollingPurpleCloudOptions }} />
     </div>
-  </div>
+  </motion.div>
 );
 
 export default HomeHero;
