d7d920a08ff27aeebf11abe7d991ce5cf5c9e990
Ugglr <carl.igelstrom@gmail.com>
2022-05-27 13:54:24 +0800

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,26 +7,45 @@ import Layout from "../components/layout/Layout";
 // Allows us to animate components when they are removed from the React tree
 import { AnimatePresence, motion } from "framer-motion";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   return (
-    <Layout>
-      <AnimatePresence
-        exitBeforeEnter
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
-        <motion.div
-          key={router.route}
-          exit={{ opacity: 0 }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.6 }}
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3L776MBWQP`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3L776MBWQP', {
+              page_path: window.location.pathname,
+            });
+                `}
+      </Script>
+
+      <Layout>
+        <AnimatePresence
+          exitBeforeEnter
+          onExitComplete={() => window.scrollTo(0, 0)}
         >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+          <motion.div
+            key={router.route}
+            exit={{ opacity: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.6 }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
