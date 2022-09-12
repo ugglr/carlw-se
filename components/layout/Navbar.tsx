0d9c6d7692242e71b8a40545df4fb3e2b184287e
Ugglr <carl.igelstrom@gmail.com>
2022-09-12 22:58:57 +0800

    navbar updated

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SiGithub, SiLinkedin, SiTwitter } from "react-icons/si";
 
 import styles from "./Navbar.module.scss";
 
@@ -10,20 +11,55 @@ const Navbar = () => {
           <a>CW.</a>
         </Link>
       </div>
-      <div className={styles.linksContainer}>
-        <div className={styles.link}>
-          <Link href="/blog">
-            <a>blog</a>
-          </Link>
+      <div className={styles.rightContainer}>
+        <div className={styles.linksContainer}>
+          <div className={styles.link}>
+            <Link href="/about">
+              <a>about.</a>
+            </Link>
+          </div>
+
+          <div className={styles.linkMargin} />
+
+          <div className={styles.link}>
+            <Link href="/blog">
+              <a>blog.</a>
+            </Link>
+          </div>
+
+          <div className={styles.linkMargin} />
+
+          <div className={styles.link}>
+            <Link href="/projects">
+              <a>current projects.</a>
+            </Link>
+          </div>
         </div>
 
         <div className={styles.linkMargin} />
 
-        <div className={styles.link}>
-          <Link href="/projects">
-            <a>current projects</a>
-          </Link>
-        </div>
+        <a href="https://github.com/ugglr" target="_blank" rel="noreferrer">
+          <SiGithub className={styles.socialIcon} />
+        </a>
+
+        <div className={styles.linkMargin} />
+        <a
+          href="https://www.linkedin.com/in/igelstrom/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiTwitter className={styles.socialIcon} />
+        </a>
+
+        <div className={styles.linkMargin} />
+
+        <a
+          href="https://www.linkedin.com/in/igelstrom/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <SiLinkedin className={styles.socialIcon} />
+        </a>
       </div>
     </div>
   );
