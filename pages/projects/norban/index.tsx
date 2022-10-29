a8346b20315fe2a13876f8456fe8ad88a043cc9e
Ugglr <carl.igelstrom@gmail.com>
2022-10-29 23:04:28 +0800

    added unique titles to norban and brewtime pages.

--- pages/projects/norban/index.tsx
+++ pages/projects/norban/index.tsx
@@ -10,7 +10,7 @@ const Norban: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>carlw.se - Fullstack engineer</title>
+        <title>carlw.se - Fullstack engineer | norban</title>
         <meta name="description" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
